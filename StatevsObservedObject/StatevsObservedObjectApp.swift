6750f8fa5a09560aa0da197d0f1c927296b5555d
Fatih Durmaz <fatih.durmaz@outlook.com>
2023-12-26 13:52:42 +0300

    Initial Commit

---
+++ StatevsObservedObject/StatevsObservedObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatevsObservedObjectApp.swift
+//  StatevsObservedObject
+//
+//  Created by Fatih Durmaz on 26.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct StatevsObservedObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
